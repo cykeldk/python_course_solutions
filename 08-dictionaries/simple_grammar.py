0589d1f39d402d42cf1969053c9fd46e87e7d165
Michael Rulle <michael.1978@gmail.com>
2017-02-16 22:02:46 +0100

    added simple_grammar.py

---
+++ 08-dictionaries/simple_grammar.py
@@ -0,0 +1,24 @@
+import random
+
+grammar = {
+        "_S"  : ["_N", "_V"],
+        "_N"  : ["developer", "teacher", "student"],
+        "_V"  : ["learns", "trains", "tests", "is", "studies", "asks"]
+}
+
+def make_sentence(index=0, sentence=[]):
+    if index > len(grammar['_S'])-1:
+        print('done')
+        return(' '.join(sentence))
+    else:
+        print('should add stuff')
+        key = grammar['_S'][index]
+        sentence.append(random.choice(grammar[key]))
+        print('sentence so far: {0}'.format(sentence))
+        return make_sentence(index+1, sentence)
+
+
+
+# print(make_sentence())
+# val2 = make_sentence()
+print('prog result: {0}'.format(make_sentence()))
