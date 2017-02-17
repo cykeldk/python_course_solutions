5d1221b9139856794f0da1b0a3219f12c3607c49
Michael Rulle <michael.1978@gmail.com>
2017-02-17 01:04:14 +0100

    grammar is difficult (08)

--- 08-dictionaries/simple_grammar.py
+++ 08-dictionaries/simple_grammar.py
@@ -1,20 +1,20 @@
 import random
 
 grammar = {
-        "_S"  : ["_N", "_V"],
+        "_S"  : ["_N", "_V", "_N"],
         "_N"  : ["developer", "teacher", "student"],
         "_V"  : ["learns", "trains", "tests", "is", "studies", "asks"]
 }
 
 def make_sentence(index=0, sentence=[]):
     if index > len(grammar['_S'])-1:
-        print('done')
+        #print('done')
         return(' '.join(sentence))
     else:
-        print('should add stuff')
+        #print('should add stuff')
         key = grammar['_S'][index]
         sentence.append(random.choice(grammar[key]))
-        print('sentence so far: {0}'.format(sentence))
+        #print('sentence so far: {0}'.format(sentence))
         return make_sentence(index+1, sentence)
 
 
